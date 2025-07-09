d1aee21a19acab2af6e550cc05c4938cc90b2c5b
Batchaaaaan <ctian.arreola22@gmail.com>
2025-07-09 16:27:16 +0800

    Added a new randomizing function
    Added the getRandomColor() function wherein
    it gets a random color to change the hovered
    grid square background color

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function checkGridSize(gridSize) {
 
 
 function changeGridColor(gridItem){
-    gridItem.style.backgroundColor='lightblue'
+    gridItem.style.backgroundColor=getRandomColor();
 }
 
 
